a04650f12ca3d0ddaf4b18e0219ffc9eedb4496d
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 12:31:30 -0700

    Add project files.

---
+++ MathMethodsAssignment/MathMethods1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethodsAssignment
+{
+    public class MathMethods1
+    {
+        //2 methods that take one integer parameter and return an integer
+        public void DivideByTwo(int number) //method divides and shows result
+        {
+            int result = number / 2;
+            Console.WriteLine("result of divide: " + result);
+        }
+
+        public void DivideByTwo(int number, out int result) //method overload, uses output parameters
+        {
+            result = number / 2;
+        }
+    }
+}
---
+++ MathMethodsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating class
+            MathMethods1 mathMethods1 = new MathMethods1();
+
+            Console.WriteLine("Please enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //calls method DivideByTwo from the class above 
+            mathMethods1.DivideByTwo(userInput);
+
+            int outputResult; //output parameter
+            mathMethods1.DivideByTwo(userInput, out outputResult);
+            Console.WriteLine("Output result: " + outputResult);
+            Console.ReadLine();
+
+        }
+    }
+}
